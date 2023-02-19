7adf856fc6aa91d1b185f2bf13cd6d5ed0b31374
Marshall <narutowxl@gmail.com>
2023-02-19 20:29:38 +0800

    Wallet conditionals

--- components/ui/navbar/Walletbar.tsx
+++ components/ui/navbar/Walletbar.tsx
@@ -1,4 +1,7 @@
 import { FC } from 'react'
+import { Menu } from '@headlessui/react'
+import Link from 'next/link'
+import { classNames } from '@utils'
 
 type WalletbarProps = {
   isLoading: boolean;
@@ -13,20 +16,88 @@ const Walletbar: FC<WalletbarProps> = ({
   connect,
   account
 }) => {
-  console.log('Is Loading: ', isLoading)
-  console.log('Is Installed: ', isInstalled)
+  if (isLoading) {
+    return (
+      <div>
+        <button
+          onClick={() => {}}
+          type="button"
+          className="inline-flex items-center px-3 py-1.5 border border-transparent text-xs font-medium rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >
+          Loading ...
+        </button>
+      </div>
+    )
+  }
 
-  return (
-    <button
-      onClick={() => {
-        connect()
-      }}
-      type="button"
-      className="inline-flex items-center px-3 py-1.5 border border-transparent text-xs font-medium rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-    >
-      Connect Wallet
-    </button>
-  )
+  if (account) {
+    return (
+      <Menu as="div" className="ml-3 relative">
+        <div>
+          <Menu.Button
+            className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+            <span className="sr-only">Open user menu</span>
+            <img
+              className="h-8 w-8 rounded-full"
+              src="/images/default_user_image.png"
+              alt=""
+            />
+          </Menu.Button>
+        </div>
+
+        <Menu.Items
+          className="z-10 origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <Menu.Item>
+            {() => (
+              <button
+                disabled={true}
+                className="disabled:text-gray-500 text-xs block px-4 pt-2 text-gray-700">
+                {`${account.slice(0, 5)}....${account.slice(-4)}`}
+              </button>
+            )}
+          </Menu.Item>
+          <Menu.Item>
+            {({ active }) => (
+              <Link href="/profile"
+                    className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}>
+                Profile
+              </Link>
+            )}
+          </Menu.Item>
+        </Menu.Items>
+      </Menu>
+    )
+  }
+
+  if (isInstalled) {
+    return (
+      <div>
+        <button
+          onClick={() => {
+            connect()
+          }}
+          type="button"
+          className="inline-flex items-center px-3 py-1.5 border border-transparent text-xs font-medium rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >
+          Connect Wallet
+        </button>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <button
+          onClick={() => {
+            window.open('https://metamask.io', '_ blank')
+          }}
+          type="button"
+          className="inline-flex items-center px-3 py-1.5 border border-transparent text-xs font-medium rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >
+          No Wallet
+        </button>
+      </div>
+    )
+  }
 }
 
 export default Walletbar
