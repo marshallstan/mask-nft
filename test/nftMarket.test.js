8e38375364220c4843076a661389d9d3726d4253
Marshall <narutowxl@gmail.com>
2023-02-21 21:49:38 +0800

    Listing Price Test

--- test/nftMarket.test.js
+++ test/nftMarket.test.js
@@ -1,7 +1,9 @@
 const NftMarket = artifacts.require('NftMarket')
+const { ethers } = require('ethers')
 
 contract('NftMarket', accounts => {
   let _contract = null
+  let _nftPrice = ethers.utils.parseEther('0.3').toString()
 
   before(async () => {
     _contract = await NftMarket.deployed()
@@ -11,7 +13,7 @@ contract('NftMarket', accounts => {
     const tokenURI = 'https://test.com'
 
     before(async () => {
-      await _contract.mintToken(tokenURI, {
+      await _contract.mintToken(tokenURI, _nftPrice, {
         from: accounts[0]
       })
     })
@@ -28,12 +30,17 @@ contract('NftMarket', accounts => {
 
     it('should not be possible to create a NFT with used tokenURI', async () => {
       try {
-        await _contract.mintToken(tokenURI, {
+        await _contract.mintToken(tokenURI, _nftPrice, {
           from: accounts[0]
         })
       } catch (error) {
         assert(error, 'NFT was minted with previously used tokenURI')
       }
+    })
+
+    it('should have one listed item', async () => {
+      const listedItemCount = await _contract.listedItemsCount()
+      assert.equal(listedItemCount.toNumber(), 1, 'Listed items count is not 1')
     })
   })
 })
