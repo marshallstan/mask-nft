1a1e38ac6050493483bfad971b2454c5c642cc9e
Marshall <narutowxl@gmail.com>
2023-02-22 16:59:18 +0800

    _beforeTokenTransfer bug fixed

--- contracts/NftMarket.sol
+++ contracts/NftMarket.sol
@@ -105,9 +105,10 @@ contract NftMarket is ERC721URIStorage {
   function _beforeTokenTransfer(
     address from,
     address to,
-    uint tokenId
+    uint tokenId,
+    uint256 batchSize
   ) internal virtual override {
-    super._beforeTokenTransfer(from, to, tokenId);
+    super._beforeTokenTransfer(from, to, tokenId, batchSize);
 
     // minting token
     if (from == address(0)) {
