376cce16eca7f37c107e04823bf878f9e05b5836
Marshall <narutowxl@gmail.com>
2023-02-25 16:23:32 +0800

    Use listed Nfts

--- components/hooks/web3/index.ts
+++ components/hooks/web3/index.ts
@@ -13,3 +13,10 @@ export const useNetwork = () => {
 
   return { network: swrRes }
 }
+
+export const useListedNfts = () => {
+  const hooks = useHooks()
+  const swrRes = hooks.useListedNfts()
+
+  return { nfts: swrRes }
+}
--- components/hooks/web3/setupHooks.ts
+++ components/hooks/web3/setupHooks.ts
@@ -1,10 +1,12 @@
 import { Web3Dependencies } from '@/types/hooks'
 import { hookFactory as createAccountHook, UseAccountHook } from './useAccount'
 import { hookFactory as createNetworkHook, UseNetworkHook } from './useNetwork'
+import { hookFactory as createListedNftsHook, UseListedNftsHook } from './useListedNfts'
 
 export type Web3Hooks = {
   useAccount: UseAccountHook
   useNetwork: UseNetworkHook
+  useListedNfts: UseListedNftsHook
 }
 
 export type SetupHooks = {
@@ -13,5 +15,6 @@ export type SetupHooks = {
 
 export const setupHooks: SetupHooks = deps => ({
   useAccount: createAccountHook(deps),
-  useNetwork: createNetworkHook(deps)
+  useNetwork: createNetworkHook(deps),
+  useListedNfts: createListedNftsHook(deps)
 })
