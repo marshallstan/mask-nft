a30681b456d05a6eb17bf49ef8f2bda7114512d9
Marshall <narutowxl@gmail.com>
2023-02-28 16:46:19 +0800

    Get contract server side

--- pages/api/utils.ts
+++ pages/api/utils.ts
@@ -1,5 +1,7 @@
+import { ethers } from 'ethers'
 import { NextApiRequest, NextApiResponse } from 'next'
 import { Session, withIronSession } from 'next-iron-session'
+import { NftMarketContract } from '@/types/nftMarketContract'
 import contract from '../../public/contracts/NftMarket.json'
 
 const NETWORKS = {
@@ -8,6 +10,7 @@ const NETWORKS = {
 
 type NETWORK = typeof NETWORKS;
 
+const abi = contract.abi
 const targetNetwork = process.env.NEXT_PUBLIC_NETWORK_ID as keyof NETWORK
 
 export const contractAddress = contract['networks'][targetNetwork]['address']
@@ -26,8 +29,17 @@ export const addressCheckMiddleware = async (
   req: NextApiRequest & { session: Session },
   res: NextApiResponse
 ) => {
-  return new Promise((resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
     const message = req.session.get('message-session')
+    const provider = new ethers.providers.JsonRpcProvider('http://127.0.0.1:7545')
+    const contract = new ethers.Contract(
+      contractAddress,
+      abi,
+      provider
+    ) as unknown as NftMarketContract
+
+    const name = await contract.name()
+    console.log('name ===> ', name)
 
     if (message) {
       resolve('Correct Address')
