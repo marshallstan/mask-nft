35028a76b4f1e79a5d0cd914976deb334cbcada1
Marshall <narutowxl@gmail.com>
2023-03-02 14:30:17 +0800

    UI change

--- components/ui/layout/BaseLayout.tsx
+++ components/ui/layout/BaseLayout.tsx
@@ -5,14 +5,14 @@ const BaseLayout = (
   { children }: { children: ReactNode }
 ): JSX.Element => {
   return (
-    <>
+    <div className="h-screen">
       <Navbar />
-      <div className="py-16 bg-gray-50 overflow-hidden min-h-screen">
+      <div className="pt-24 pb-16 bg-gray-50 overflow-hidden min-h-full">
         <div className="max-w-7xl mx-auto px-4 space-y-8 sm:px-6 lg:px-8">
           {children}
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
