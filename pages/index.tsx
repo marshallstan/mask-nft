923ccad5be21257182159b299be337b819c7745a
Marshall <narutowxl@gmail.com>
2023-03-02 13:19:07 +0800

    Display wrong network

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
 import { BaseLayout, NftList } from '@components/ui'
+import { useNetwork } from '@components/hooks/web3'
+import { ExclamationCircleIcon } from '@heroicons/react/20/solid'
 
 const Home = () => {
+  const { network } = useNetwork()
   return (
     <BaseLayout>
       <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
@@ -14,7 +17,28 @@ const Home = () => {
               Mint a NFT to get unlimited ownership forever!
             </p>
           </div>
-          <NftList />
+          {
+            network.isConnectedToNetwork ?
+              <NftList /> :
+              <div className="rounded-md bg-yellow-50 p-4 mt-10">
+                <div className="flex">
+                  <div className="flex-shrink-0">
+                    <ExclamationCircleIcon className="h-5 w-5 text-yellow-400" aria-hidden="true" />
+                  </div>
+                  <div className="ml-3">
+                    <h3 className="text-sm font-medium text-yellow-800">Attention needed</h3>
+                    <div className="mt-2 text-sm text-yellow-700">
+                      <p>
+                        {network.isLoading ?
+                          'Loading...' :
+                          `Connect to ${network.targetNetwork}`
+                        }
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+          }
         </div>
       </div>
     </BaseLayout>
