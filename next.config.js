2d8b53be2d16a9f3540ef2e3a3ecfbb25eabc518
Marshall <narutowxl@gmail.com>
2023-03-03 12:17:38 +0800

    images remotePatterns

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  swcMinify: true
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'gateway.pinata.cloud',
+        port: '',
+        pathname: '/ipfs/**'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
